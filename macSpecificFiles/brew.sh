3027d4fdcfae0da7022c217127011484ab4f743b
Tristan Krass <tristankrass@gmail.com>
2019-05-08 10:44:30 +0300

    Added brew files

---
+++ macSpecificFiles/brew.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install firefox
+brew cask install virtualbox
+brew cask install calibre
+brew cask install sourcetree
+brew cask install spotify
+brew cask install discord
+brew cask install google-backup-and-sync
+brew cask install gimp
+brew cask install hyperdock
+brew cask install divvy
+brew install zsh
+brew cask install visual-studio-code
+
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
